65352b9949c7721b7e8abf474610bbbf6c0867f1
muhammadboburshoh <muhammadbobur600@gmail.com>
2022-04-23 04:15:31 +0500

    delete redundant code

--- routes/shop.js
+++ routes/shop.js
@@ -1,6 +1,4 @@
-const path = require('path');
 const express = require('express');
-const rootDir = require('../util/path');
 const adminData = require("./admin")
 
 const router = express.Router();
