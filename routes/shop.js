6b24f3513486ffe0c6c0eccd87f8ff4af86dddf5
muhammadboburshoh <muhammadbobur600@gmail.com>
2022-04-23 09:22:16 +0500

    pug view engine to ejs

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,22 @@
+const path = require('path');
+
 const express = require('express');
-const adminData = require("./admin")
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  const products = adminData.products
-  res.render("shop", { prods: products, pageTitle: "Shop" ,path: "/"})
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
