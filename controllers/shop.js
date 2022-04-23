ca457dacbe70cc3dc30a43e7ee51d89bac6427c3
muhammadboburshoh <muhammadbobur600@gmail.com>
2022-04-23 20:45:55 +0500

    Add product card data

--- controllers/shop.js
+++ controllers/shop.js
@@ -33,9 +33,21 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
+  Cart.getCart(cart => {
+    Product.fetchAll(products => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProductData = cart.prodcuts.find(prod => prod.id === product.id)
+        if(cartProductData) {
+          cartProducts.push({productData: product, qty: cartProductData.qty});
+        }
+      }
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: cartProducts
+      });
+    });
   });
 };
 
