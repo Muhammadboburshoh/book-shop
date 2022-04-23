1621bc15120660f7fe2604cb5a75f95adf10ae76
muhammadboburshoh <muhammadbobur600@gmail.com>
2022-04-23 18:09:24 +0500

    add edit product page

--- models/product.js
+++ models/product.js
@@ -18,7 +18,8 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -26,12 +27,22 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
     getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
+      if(this.id) {
+        const existingProductIndex = products.findIndex(prod => prod.id === this.id);
+        const updateProducts = [...products];
+        updateProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updateProducts), err => {
+          console.log(err);
+        });
+      }
+      else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
+      }
     });
   }
 
