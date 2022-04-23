47eee30147fa05039609c34764eb189d35d15508
muhammadboburshoh <muhammadbobur600@gmail.com>
2022-04-23 12:24:08 +0500

    add all pages

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('add-product', {
+  res.render('admin/add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     formsCSS: true,
@@ -9,22 +9,24 @@ exports.getAddProduct = (req, res, next) => {
     activeAddProduct: true
   });
 };
-
+  
 exports.postAddProduct = (req, res, next) => {
-  const product = new Product(req.body.title);
+  const title = req.body.title
+  const price = req.body.price
+  const imageUrl = req.body.imageUrl
+  const description = req.body.description
+
+  const product = new Product(title, price, imageUrl, description);
   product.save();
   res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
-    res.render('shop', {
+    res.render('admin/products', {
       prods: products,
-      pageTitle: 'Shop',
-      path: '/',
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
+      pageTitle: 'Admin Products',
+      path: '/admin/products'
     });
   });
-};
+}
